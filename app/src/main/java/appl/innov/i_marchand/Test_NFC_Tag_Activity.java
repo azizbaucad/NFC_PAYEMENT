42100ee51f313cbeece266a6b3047e172ba9bc1f
balepro <baabdouaziz898@gmail.com>
2022-03-17 18:08:42 +0000

    paywithnfc

--- app/src/main/java/appl/innov/i_marchand/Test_NFC_Tag_Activity.java
+++ app/src/main/java/appl/innov/i_marchand/Test_NFC_Tag_Activity.java
@@ -2,13 +2,33 @@ package appl.innov.i_marchand;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Test_NFC_Tag_Activity extends AppCompatActivity {
+
+    // déclarer les variables
+    //private Button payNFC ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test__n_f_c__tag_);
+
+        // Initialisé les variables
+       Button payNFC = (Button)findViewById(R.id.btn_pay_nfc);
+
+        payNFC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Test_NFC_Tag_Activity.this, ValidePayementActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        });
     }
 }
