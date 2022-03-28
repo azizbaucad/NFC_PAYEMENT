1bcc3d86e85f98d4c81a972074e6afb58854905e
balepro <baabdouaziz898@gmail.com>
2022-03-28 17:55:15 +0000

    commit

--- app/src/main/java/appl/innov/i_marchand/Test_NFC_Tag_Activity.java
+++ app/src/main/java/appl/innov/i_marchand/Test_NFC_Tag_Activity.java
@@ -18,6 +18,9 @@ import org.ksoap2.serialization.PropertyInfo;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 
 import appl.innov.i_marchand.helper.DatabaseHelper;
 import nl.bryanderidder.themedtogglebuttongroup.ThemedButton;
@@ -40,7 +43,7 @@ public class Test_NFC_Tag_Activity extends AppCompatActivity {
        myDb = new DatabaseHelper(this);
 
        soldeText = findViewById(R.id.soldeId);
-       getAllListAccount();
+       //getAllListAccount();
        // Fin de l'initialisation des vraiables
         // Button payNFC pour étendre le paiement par NFC
         payNFC.setOnClickListener(new View.OnClickListener() {
@@ -69,43 +72,14 @@ public class Test_NFC_Tag_Activity extends AppCompatActivity {
     public void getAllListAccount() {
         Cursor res = myDb.getAllData();
         if (res.moveToLast()) {
-            //soldeText.setText("0125666");
             // http://50.116.97.25:8080/cash-ws/CashWalletServiceWS?wsdl // server de test
             // String URL = "http://ibusinesscompanies.com:18080/cash-ws/CashWalletServiceWS?wsdl"; // serveur de prod
             String URL = "http://50.116.97.25:8080/cash-ws/CashWalletServiceWS?wsdl"; // serveur de test
             String NAMESPACE = "http://runtime.services.cash.innov.sn/";
-
             String SOAP_ACTION = "";
             String METHOD_NAME = "getAllListAccount";
-            //soldeText.setText("265" + getResources().getString(R.string.currency));
-            /* Test d'une autre méthode de récupération */
-            // Initialize soap request + add parameters
             SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
-            // Use this to add parameters
-            request.addProperty("sessionId","170536");
-            //Declare the version of the SOAP request
-            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-            envelope.setOutputSoapObject(request);
-            //Needed to make the internet call
-            HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
-            try {
-                //this is the actual part that will call the webservices
-                androidHttpTransport.call(SOAP_ACTION, envelope);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            //Get the SoapResult from the envelope body
-            SoapObject result = (SoapObject)envelope.bodyIn;
 
-            if(result !=null){
-                //soldeText.setText("101");
-                //Get the first property and change the label text
-            //soldeText.setText("001" + result.getProperty(1).toString());
-            }
-            soldeText.setText(""+result); // Erreur
-
-            /* Fin du test de la méthode de récupération */
-            /*SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
             String idSession_;
             //set the idSession retrieved
             PropertyInfo session = new PropertyInfo();
@@ -124,39 +98,35 @@ public class Test_NFC_Tag_Activity extends AppCompatActivity {
             request.addProperty(tel);
 
             try {
-                soldeText.setText("085");
+
                 SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-                //envelope.dotNet=true;
                 envelope.setOutputSoapObject(request);
                 HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
                 androidHttpTransport.call(SOAP_ACTION, envelope);
                 SoapObject result = (SoapObject) envelope.getResponse();
-                String accounts = result.getProperty(0).toString();
                 String error = result.getProperty(1).toString();
                 Log.e("result", "responceeeeeeee from ws getAllListAccount" + result);
-                //if (error.equals("0")) {
-                //ldeText.setText("01254");
-                //String accounts = result.getProperty(0).toString();
+                if (error.equals("0")) {
+
+                    String accounts = result.getProperty(0).toString();
                     String[] stringArray = accounts.split(";");
-                    String sold = stringArray[3];
+                    String sold = stringArray[4]; // propriété du webservice
                     String[] soldeArray = sold.split("=");
                     String soldeBrute = soldeArray[1];
 
                     Log.e("result", "responceeeeeeee from ws accounts" + accounts);
                     Log.e("result", "responceeeeeeee from ws solde" + soldeBrute);
-                    //soldeText.setText(soldeBrute + getResources().getString(R.string.currency));
-                   //soldeText.setText("012224444555522");
-               // }
+                    soldeText.setText(soldeBrute + getResources().getString(R.string.currency));
+                }
 
             } catch (Exception e) {
                 e.printStackTrace();
-            }*/
-
+            }
         }
     }
     // Fin de la création de la fonction getAllListAccount
     // Appel de la fonction get AllListAccount de façon Asynchrone
-    /*class AsyncCallWS2 extends AsyncTask<String, Void, String> {
+    class AsyncCallWS2 extends AsyncTask<String, Void, String> {
         private final ProgressDialog Dialog = new ProgressDialog(Test_NFC_Tag_Activity.this);
 
         @Override
@@ -177,6 +147,6 @@ public class Test_NFC_Tag_Activity extends AppCompatActivity {
             //super.onPostExecute(s);
             Dialog.dismiss();
         }
-    }*/
+    }
     // Fin de l'appel de la fonction getAllListAccount
 }
---
+++ app/src/main/java/appl/innov/i_marchand/fixerMontantActivity.java
@@ -0,0 +1,14 @@
+package appl.innov.i_marchand;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fixerMontantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fixer_montant);
+    }
+}
